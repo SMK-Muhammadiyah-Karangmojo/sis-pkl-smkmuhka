860a0e416bb36bd88f276cc7383f7b147e13cb8f
yanto <ekocahyanto007@gmail.com>
2023-12-22 04:14:17 +0700

    add phpinfo

---
+++ php-info.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Copyright (c) 2023. Yantodev - All Rights Reserved.
+ * @Author  :  yantodev
+ * mailto : ekocahyanto007@gmail.com
+ * link : https://yantodev.my.id/
+ */
+
+
+phpinfo(); ?>
