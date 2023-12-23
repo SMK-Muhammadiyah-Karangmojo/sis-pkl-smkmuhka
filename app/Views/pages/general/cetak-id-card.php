e721624668ad7b1fe4c6fb29fbb1a76816d7d209
yanto <ekocahyanto007@gmail.com>
2023-12-24 03:11:39 +0700

    fixing barcode id-card

--- app/Views/pages/general/cetak-id-card.php
+++ app/Views/pages/general/cetak-id-card.php
@@ -159,7 +159,7 @@
                     </div>
                     <div class="scan">
                         <strong>SCAN ME</strong><br/>
-                        <barcode code="<?= base_url('home/detailsiswa/') . $siswa->id; ?>" size="1.2" type="QR"
+                        <barcode code="<?= base_url() . 'home/detailsiswa/' . $siswa->id; ?>" size="1.2" type="QR"
                                  error="M" class="barcode"></barcode>
                     </div>
                 </div>
